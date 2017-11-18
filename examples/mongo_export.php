02f6b8481e7d37aab7120b42b26b24164bc837fc
wenzhihao <wenzhihao@115.com>
2017-11-18 11:01:57 +0800

    Repair mongodb key can not have special characters

--- examples/mongo_export.php
+++ examples/mongo_export.php
@@ -1,5 +1,34 @@
 <?php
-$manager = new MongoDB\Driver\Manager('mongodb://localhost:27017');
-$bulk = new \MongoDB\Driver\BulkWrite;
-$bulk->insert($_apm_export);
-$manager->executeBulkWrite('xhprof_apm.results', $bulk);
+
+if (empty($_apm_export)) {
+    return false;
+}
+
+// 新版的mongokey中不能包含`.`
+foreach($_apm_export['profile'] as $key => &$value) {
+    if (!empty($value['files'])){
+        foreach ($value['files'] as $k => $v){
+            $nk = strtr($k, ['.'=> '_']);
+            if ($nk != $k){
+                unset($value['files'][$k]);
+                $value['files'][$nk] = $v;
+            }
+        }
+    }
+
+    $nkey = strtr($key, ['.' => '_']);
+    if ($nkey != $key){
+        unset($_apm_export['profile'][$key]);
+        $_apm_export['profile'][$nkey] = $value;
+    }
+}
+
+try {
+    $manager = new MongoDB\Driver\Manager('mongodb://localhost:27017');
+    $bulk    = new \MongoDB\Driver\BulkWrite;
+    $bulk->insert($_apm_export);
+    $result = $manager->executeBulkWrite('xhprof_apm.results', $bulk);
+} catch (Exception $e) {
+    var_dump($e->getMessage());
+    exit;
+}
--- web/src/Profiles.php
+++ web/src/Profiles.php
@@ -2,6 +2,9 @@
 
 class Profiles
 {
+    /**
+     * @var Database\Mongo|Database\Mysql
+     */
     protected $_db;
 
     public function __construct($db)
